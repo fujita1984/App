8e55c08c787562fd9f573e43ec2acc039aca26f6
fujita <fujita1110@eos.ocn.ne.jp>
2026-01-03 15:12:01 +0900

    プロジェクト ファイルを追加します。

---
+++ Controllers/HskTypeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HskTypingWeb.Controllers
+{
+    public class HskTypeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using HskTypingWeb.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace HskTypingWeb.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<HskWord> HskWords => Set<HskWord>();
+    }
+}
